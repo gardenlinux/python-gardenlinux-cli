9a2a4b065f9925e8eeb495549843bcffca3f5a3d
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-08-23 13:08:18 +0200

    Add selected sbom for gl releases

--- src/python_gardenlinux_cli/commands/apt.py
+++ src/python_gardenlinux_cli/commands/apt.py
@@ -4,8 +4,9 @@ import sys
 import click
 from python_gardenlinux_lib.apt.package_repo_info import (
     compare_gardenlinux_repo_version,
+    GardenLinuxRepo
 )
-
+import re
 
 @click.group()
 def apt():
@@ -29,3 +30,15 @@ def compare(version_a, version_b):
     b = sorted([f"{r[0]} {r[2]}\n" for r in result if r[2] is not None])
 
     sys.stdout.writelines(unified_diff(a, b, fromfile=version_a, tofile=version_b))
+
+@apt.command()
+@click.option(
+    '--gardenlinux_version',
+    required=True
+)
+def selected_sbom(gardenlinux_version):
+    packages_of_relevance = re.compile('^linux-image-amd64$|^systemd$|^containerd$|^runc$|^curl$|^openssl$|^openssh-server$|^libc-bin$')
+    repo = GardenLinuxRepo(gardenlinux_version)
+    versions = repo.get_packages_versions()
+    selected_versions = sorted([f'{p[0]} {p[1]}\n' for p in versions if packages_of_relevance.match(p[0])])
+    sys.stdout.writelines(selected_versions)
