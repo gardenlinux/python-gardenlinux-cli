b1220479c4b86e78a23dcc437c56050233c35e0b
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-08-23 11:29:01 +0200

    Output compare apt versions as unified diff

--- src/python_gardenlinux_cli/commands/apt.py
+++ src/python_gardenlinux_cli/commands/apt.py
@@ -1,6 +1,10 @@
 from os import walk
+from difflib import *
+import sys
 import click
-from python_gardenlinux_lib.apt.package_repo_info import compare_gardenlinux_repo_version
+from python_gardenlinux_lib.apt.package_repo_info import (
+    compare_gardenlinux_repo_version,
+)
 
 
 @click.group()
@@ -20,4 +24,8 @@ def apt():
 )
 def compare(version_a, version_b):
     result = compare_gardenlinux_repo_version(version_a, version_b)
-    print(result)
+
+    a = sorted([f"{r[0]} {r[1]}\n" for r in result if r[1] is not None])
+    b = sorted([f"{r[0]} {r[2]}\n" for r in result if r[2] is not None])
+
+    sys.stdout.writelines(unified_diff(a, b, fromfile=version_a, tofile=version_b))
