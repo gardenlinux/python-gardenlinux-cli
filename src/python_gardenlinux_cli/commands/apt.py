19605b4a0517437aaac264ed426ba2d532c03cda
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-08-26 12:33:26 +0200

    wip commands to view versions of packages in apt repo

--- src/python_gardenlinux_cli/commands/apt.py
+++ src/python_gardenlinux_cli/commands/apt.py
@@ -42,3 +42,38 @@ def selected_sbom(gardenlinux_version):
     versions = repo.get_packages_versions()
     selected_versions = sorted([f'{p[0]} {p[1]}\n' for p in versions if packages_of_relevance.match(p[0])])
     sys.stdout.writelines(selected_versions)
+
+@apt.command()
+@click.option(
+    '--package_name',
+    required=True
+)
+@click.option(
+    '--gardenlinux_version',
+    required=True
+)
+def package_version(package_name, gardenlinux_version):
+    repo = GardenLinuxRepo(gardenlinux_version)
+    res = repo.get_package_version_by_name(package_name)
+    # fixme: Is this a fair assumption? We're not doing a substring matching here, so we will only get exactly 0 or one match, right?
+    assert len(res) == 1, f"Expected only one package with name {package_name} in version {gardenlinux_version}, but got {len(res)} results."
+    (name, version) = res[0]
+    print(f"{name} {version}")
+
+@apt.command()
+@click.option(
+    '--package_name',
+    required=True
+)
+def package_version_in_all_recent(package_name):
+    # similar to 'all suites' in the debian package tracker https://packages.debian.org/search?suite=all&searchon=names&keywords=wget
+    # todo: get most recent versions from api
+    recent_versions = ['1592.1', '1592.0', '1443.10', '1443.9']
+    for gardenlinux_version in recent_versions:
+        repo = GardenLinuxRepo(gardenlinux_version)
+        res = repo.get_package_version_by_name(package_name)
+        # fixme: Is this a fair assumption? We're not doing a substring matching here, so we will only get exactly 0 or one match, right?
+        assert len(res) == 1, f"Expected only one package with name {package_name} in version {gardenlinux_version}, but got {len(res)} results."
+        (name, version) = res[0]
+        print(gardenlinux_version)
+        print(f"  {name} {version}")
