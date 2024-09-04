9d5c555c8319bd616920de47721aa94e7a99b92f
Malte Münch <muench@b1-systems.de>
2024-09-04 09:24:39 +0200

    Adds option to push from tarball
    Signed-off-by: Malte Münch <muench@b1-systems.de>

--- src/python_gardenlinux_cli/commands/oci.py
+++ src/python_gardenlinux_cli/commands/oci.py
@@ -1,10 +1,14 @@
 # my_project/commands/image.py
 import json
+import shutil
+import tarfile
+import tempfile
 import os
 import click
 import oras.client
 import oras.container
 from python_gardenlinux_lib.oras.registry import GlociRegistry, setup_registry
+from python_gardenlinux_lib.features import parse_features
 
 from typing import Optional
 
@@ -19,8 +23,6 @@ def oci(ctx, insecure):
     ctx.ensure_object(dict)
 
     ctx.obj["insecure"] = insecure
-
-
 
 
 @oci.command()
@@ -87,9 +89,73 @@ def push(
     private_key,
     public_key,
 ):
+    """push local build artifacts to a registry"""
     if not build_output:
         build_output = f"{gardenlinux_root}/.build"
 
+    container_name = f"{container_name}:{version}"
+    oci_metadata = parse_features.get_oci_metadata(
+        cname, version, architecture, gardenlinux_root
+    )
+    registry = setup_registry(
+        container_name,
+        insecure=ctx.obj["insecure"],
+        private_key=private_key,
+        public_key=public_key,
+    )
+    registry.push_image_manifest(
+        architecture, cname, version, build_output, oci_metadata
+    )
+    click.echo(f"Pushed {container_name}")
+
+
+@oci.command()
+@click.option(
+    "--container",
+    "container_name",
+    required=True,
+    type=click.Path(),
+    help="Container Name",
+)
+@click.option(
+    "--architecture",
+    required=True,
+    type=click.Path(),
+    help="Target Image CPU Architecture",
+)
+@click.option(
+    "--cname", required=True, type=click.Path(), help="Canonical Name of Image"
+)
+@click.option("--version", required=True, type=click.Path(), help="Version of Image")
+@click.option(
+    "--private_key",
+    required=False,
+    type=click.Path(),
+    help="Path to private key to use for signing",
+    default="cert/oci-sign.key",
+    show_default=True,
+)
+@click.option(
+    "--public_key",
+    required=False,
+    type=click.Path(),
+    help="Path to public key to use for verification of signatures",
+    default="cert/oci-sign.crt",
+    show_default=True,
+)
+@click.option("--tar", required=True, help="path to the build-tarball")
+@click.pass_context
+def push_from_tarball(
+    ctx,
+    container_name,
+    architecture,
+    cname,
+    version,
+    tar,
+    private_key,
+    public_key,
+):
+    """push artifacts from a tarball to a registry"""
     container_name = f"{container_name}:{version}"
     registry = setup_registry(
         container_name,
@@ -97,8 +163,30 @@ def push(
         private_key=private_key,
         public_key=public_key,
     )
-    registry.push_image_manifest(architecture, cname, version, gardenlinux_root, build_output)
-    click.echo(f"Pushed {container_name}")
+    assert tar.endswith(".tar.xz")
+    fullname = os.path.basename(tar).removesuffix(".tar.xz")
+    tmpdir = tempfile.mkdtemp()
+    try:
+        tar_obj = tarfile.open(tar)
+        tar_obj.extractall(filter="data", path=tmpdir)
+        tar_obj.close()
+    except (OSError, tarfile.FilterError, tarfile.TarError) as e:
+        print("Failed to extract tarball", e)
+        shutil.rmtree(tmpdir, ignore_errors=True)
+        exit(1)
+
+    build_output = f"{tmpdir}/{fullname}"
+    try:
+        oci_metadata = parse_features.get_oci_metadata_from_fileset(
+            os.listdir(build_output), architecture
+        )
+        registry.push_image_manifest(
+            architecture, cname, version, build_output, oci_metadata
+        )
+    except Exception as e:
+        shutil.rmtree(tmpdir, ignore_errors=True)
+        exit(1)
+    shutil.rmtree(tmpdir, ignore_errors=True)
 
 
 @oci.command()
