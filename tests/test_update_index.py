4cc7dda9d5cce979903782bbed8625a147e9f323
Paul Hildebrandt <hildebrandt@b1-systems.de>
2024-11-27 23:38:34 +0100

    working e2e update index
    Signed-off-by: Paul Hildebrandt <hildebrandt@b1-systems.de>

---
+++ tests/test_update_index.py
@@ -0,0 +1,57 @@
+import pytest
+from click.testing import CliRunner
+import sys
+
+sys.path.append("src")
+
+from glcli import cli
+
+
+CONTAINER_NAME_ZOT_EXAMPLE = "localhost:5000/gardenlinux"
+
+
+@pytest.mark.usefixtures("zot_session")
+@pytest.mark.parametrize(
+    "version, cname, arch",
+    [
+        ("today", "aws-gardener_prod", "arm64"),
+        ("today", "aws-gardener_prod", "amd64"),
+        ("today", "gcp-gardener_prod", "arm64"),
+        ("today", "gcp-gardener_prod", "amd64"),
+        ("today", "azure-gardener_prod", "arm64"),
+        ("today", "azure-gardener_prod", "amd64"),
+        ("today", "openstack-gardener_prod", "arm64"),
+        ("today", "openstack-gardener_prod", "amd64"),
+        ("today", "openstackbaremetal-gardener_prod", "arm64"),
+        ("today", "openstackbaremetal-gardener_prod", "amd64"),
+        ("today", "metal-kvm_dev", "arm64"),
+        ("today", "metal-kvm_dev", "amd64"),
+    ],
+)
+def test_update_index(version, arch, cname):
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "update-index",
+            "--container",
+            CONTAINER_NAME_ZOT_EXAMPLE,
+            "--version",
+            version,
+            "--insecure",
+            "True",
+            "--manifest_file",
+            "manifest-entry.json",
+        ],
+        catch_exceptions=False,
+    )
+    if result.exit_code != 0:
+        print(f"Exit Code: {result.exit_code}")
+        print(f"Output: {result.output}")
+        if result.exception:
+            print(result.exception)
+        try:
+            print(f"Output: {result.stderr}")
+        except ValueError:
+            print("No stderr captured.")
+    assert result.exit_code == 0
